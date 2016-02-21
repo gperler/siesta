1f9f5cdf4efabb00f391a018972a6258ee74cb5c
gperler <gregor.mueller@gmail.com>
2016-02-21 19:48:54 +0100

    123

--- src/siestaphp/migrator/Migrator.php
+++ src/siestaphp/migrator/Migrator.php
@@ -92,7 +92,7 @@ class Migrator
 
             $this->connection->disableForeignKeyChecks();
             foreach ($statementList as $statement) {
-                $this->logger->info("Executing " . $statement);
+                $this->logger->warning("Executing " . $statement);
                 $this->connection->query($statement);
             }
             $this->connection->enableForeignKeyChecks();
